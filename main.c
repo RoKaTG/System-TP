9dd6f698b963b5f66314b4a57413720643b3dd84
RoKaT <msiliniyassine@gmail.com>
2021-11-24 15:23:00 +0100

    Partie 1 finie et début de la partie 2

--- main.c
+++ main.c
@@ -7,7 +7,49 @@
 #include <errno.h>
 #include <dirent.h>
 #include <fcntl.h>
-#include <pthread.h>
+#define CHUNK 128
+
+#define AND &&
+#define OR ||
+#define ISNOT !=
+#define NOT !
+#define then
+
+/**         Partie 1
+  Copie le contenue d'un fichier source dans un fichier destination
+  @param char *src
+  @param char *dest
+  @return void
+*/
+void copy(char *src, char *dest) {
+  int fileS = open(src, O_RDONLY);
+  int fileD = open(dest, O_CREAT | O_WRONLY);
+  char buffer[CHUNK];
+  size_t n;
+  if (fileS < 0 || fileD < 0) {
+    exit(EXIT_FAILURE);
+  }
+  else {
+    while ((n = read(fileS, buffer, CHUNK)) > 0) {
+      write(fileD, buffer, n);
+    }
+  }
+  close(fileS);
+  close(fileD);
+}
+
+void perm(char *src, char *dest) {
+  struct stat state;
+  stat(src, &state);
+  chmod(dest, state.st_mode);
+}
+
+
+
+
+
+
+
 
 int copyfile(const char *iname, const char *oname) {
   int idesc = open(iname, O_RDONLY);
